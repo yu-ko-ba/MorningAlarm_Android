d3c9495ddf06a960834e620be093bd4d6c5b481b
yu-ko-ba <lily_lime_mint@outlook.com>
2021-08-15 11:10:53 +0900

    設定からサーバーのIPアドレスとポート番号を変更できるようにした

--- app/src/main/java/com/example/morningalarm/android/MainActivity.kt
+++ app/src/main/java/com/example/morningalarm/android/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.morningalarm.android
 
+import android.app.AlarmManager
+import android.app.AlertDialog
 import android.app.TimePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +13,10 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
 import com.example.morningalarm.android.databinding.ActivityMainBinding
+import com.example.morningalarm.android.databinding.DialogSettingsBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -53,7 +59,30 @@ class MainActivity : AppCompatActivity() {
                 MorningAlarmManager.firstFragment?.setAdapter(MorningAlarmManager.get().getJSONObject("data"))
                 true
             }
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val view = this.layoutInflater.inflate(R.layout.dialog_settings, null)
+                AlertDialog.Builder(this)
+                    .setTitle("設定")
+                    .setView(view)
+                    .setPositiveButton("OK", DialogInterface.OnClickListener { dialog, id ->
+                        val serverAddress = view.findViewById<EditText>(R.id.serverAddress).text.toString()
+                        val portNumber = view.findViewById<EditText>(R.id.portNumber).text.toString()
+
+                        if (serverAddress != "") {
+                            MorningAlarmManager.serverAddress = serverAddress
+                        }
+                        if (portNumber != "") {
+                            MorningAlarmManager.portNumber = portNumber
+                        }
+
+                        MorningAlarmManager.firstFragment?.setAdapter()
+                    })
+                    .setNegativeButton("Chancel", DialogInterface.OnClickListener { dialog, id ->
+                        dialog.cancel()
+                    })
+                    .show()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
